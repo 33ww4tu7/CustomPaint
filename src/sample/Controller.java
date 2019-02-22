7ea82b82c8e3759b40c049bbfb980c8cda442a2f
Pavel <fandopavel@gmail.com>
2019-02-23 01:26:33 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+}
